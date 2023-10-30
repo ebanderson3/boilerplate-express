8f33b73f715969f01661119cdfa122cc25ffd6b2
eband <ebanderson@mail.lipscomb.edu>
2023-10-30 14:26:59 -0500

    Added middleware to myApp.js

--- myApp.js
+++ myApp.js
@@ -2,9 +2,11 @@ let express = require('express');
 let app = express();
 
 
+app.use("/public", express.static(__dirname + "/public"));
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/views/index.html")
 }); 
+
 
 console.log("Hello World")
 
